31cfe4292e8900471045039e0f3e4f2adc6af830
jhanvi006 <jhanvikankhara06@gmail.com>
2022-06-12 00:40:58 +0530

    Added: new properties added to class

--- EmployeePayrollService.js
+++ EmployeePayrollService.js
@@ -1,21 +1,33 @@
 class EmployeePayrollData{
     id;
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     get name(){ return this._name; }
     set name(name){ this._name = name; }
 
     toString(){
-        return "id: "+this.id+" name: "+this.name+" salary:"+this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" : 
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "id: "+this.id+", name: "+this.name+", salary:"+this.salary+
+        ", gender: "+this.gender+", start date: "+empDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
+employeePayrollData.id = 0;
 employeePayrollData.name = "John";
 console.log(employeePayrollData.toString());
+
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
